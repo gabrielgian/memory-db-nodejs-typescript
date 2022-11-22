dac04b0b4ede9e8ee6240296a6d934e5998b5065
gabrielgian <gabriel.tessaroli@gmail.com>
2022-11-22 14:21:42 -0300

    feat: add backup feat

--- src/Database.ts
+++ src/Database.ts
@@ -1,8 +1,14 @@
-import Collection, { CollectionNameType } from './Collection';
+import { open } from 'node:fs/promises';
+import { Stream } from 'node:stream';
+import { Worker, isMainThread, workerData } from 'node:worker_threads';
+import mixin from 'merge-descriptors';
+import { JsonStreamStringify } from 'json-stream-stringify';
 
+import Collection, { CollectionNameType } from './Collection';
 class Database {
   name: string;
   collections: Map<string, Collection>;
+  backups: Array<string>;
 
   /**
    * Creates a new database with a specified name
@@ -12,6 +18,7 @@ class Database {
   constructor(name: string) {
     this.name = name;
     this.collections = new Map();
+    this.backups = [];
   }
 
   /**
@@ -74,6 +81,70 @@ class Database {
 
     return this.collections.delete(name);
   }
+
+  /**
+   * Creates a backup and save it to a json file. This task is performed by a worker.
+   *
+   * @param backupFileName backup file name (default: {@link Database.generateNewBackupFileName})
+   * @returns Returns a Promise that will resolve true if the backup was created
+   */
+  async backup(backupFileName: string = this.generateNewBackupFileName()): Promise<boolean> {
+    return new Promise((resolve) => {
+      const workerData = { db: this, backupFileName };
+      const worker = new Worker(__filename, { workerData });
+      worker.on('exit', (code) => {
+        if (code === 0) {
+          this.backups.push(backupFileName);
+        }
+
+        resolve(code === 0);
+      });
+    });
+  }
+
+  /**
+   * Save the backup into a json file.
+   *
+   * @param backupFileName backup file name
+   * @returns Returns a Promise that will resolve true if the backup was created
+   */
+  private async save(backupFileName: string): Promise<void> {
+    const getCollectionStream = function* (collections: Map<string, Collection>) {
+      // eslint-disable-next-line no-loops/no-loops
+      for (const [, collection] of collections) {
+        yield new JsonStreamStringify({
+          name: collection.name,
+          values: Stream.Readable.from(collection.values, { objectMode: true }),
+        });
+      }
+    };
+
+    const fd = await open(backupFileName, 'w');
+
+    const databaseJson = new JsonStreamStringify({
+      name: this.name,
+      collections: Stream.Readable.from(getCollectionStream(this.collections), { objectMode: true }),
+    });
+
+    databaseJson.pipe(fd.createWriteStream());
+
+    return new Promise((resolve) => databaseJson.on('close', () => resolve()));
+  }
+
+  /**
+   * @returns Returns a default backup name using new Date()
+   */
+  private generateNewBackupFileName(): string {
+    return `./${this.name}_${new Date().toISOString()}.json`;
+  }
+}
+
+if (!isMainThread) {
+  const { db, backupFileName } = workerData;
+
+  mixin(db, Database.prototype, false);
+
+  db.save(backupFileName);
 }
 
 export default Database;
